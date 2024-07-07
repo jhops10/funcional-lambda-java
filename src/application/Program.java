28edee9f1bf323e2fd6c1c9e05498df516c2ae3f
João Paulo <joaowais@gmail.com>
2024-07-06 21:28:09 -0300

    Criando funções que recebem funções como parâmetro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
 import entities.Product;
-import util.UpperCaseName;
+import model.services.ProductService;
+
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+
 
 public class Program {
     public static void main(String[] args) {
@@ -15,35 +15,13 @@ public class Program {
 
         list.add(new Product("TV", 900.00));
         list.add(new Product("Mouse", 50.00));
-        list.add(new Product("Tablet", 450.00));
+        list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD Case", 80.90));
 
+        ProductService ps = new ProductService();
+        double sum = ps.filteredSum(list, product -> product.getPrice() < 100.0);
 
-       //Implementação de Interface
-        //List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
-        //names.forEach(product -> System.out.println(product));
-
-
-        //Reference Method com metodo estatico
-        //List<String> names = list.stream().map(Product::staticUpperCaseName).toList();
-        //names.forEach(product -> System.out.println(product));
-
-
-        //Reference Method sem metodo estatico
-        //List<String> names = list.stream().map(Product::nonStaticUpperCaseName).toList();
-        //names.forEach(System.out::println);
-
-
-
-        //Expressão Lambda Declarada
-        //Function<Product, String> func = product -> product.getName().toUpperCase();
-        //List<String> names = list.stream().map(func).toList();
-
-
-        //Expressão Lambda Inline
-        List<String> names = list.stream().map(product -> product.getName().toUpperCase()).toList();
-        names.forEach(System.out::println);
-
+        System.out.println("Sum = " + String.format("%.2f", sum));
         //list.forEach(product -> System.out.println(product.toString()));
 
     }
