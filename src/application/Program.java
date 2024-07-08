56cda97c84188ae1e595cc532445e0de239ae6bb
João Paulo <joaowais@gmail.com>
2024-07-08 15:25:02 -0300

    Exercicio Resolvido (Stream)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,26 +1,52 @@
 package application;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import entities.Product;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Locale;
+
 
 public class Program {
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+        Locale.setDefault(Locale.US);
+        String path = "E:\\DEVELOPMENT\\Java\\Projetos\\in.txt";
 
-        Stream<Integer> st1 = list.stream().map(x -> x * 10);
-        System.out.println(Arrays.toString(st1.toArray()));
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-        int sum = list.stream().reduce(0, (x, y) -> x + y);
-        System.out.println("Sum =" + sum);
+            List<Product> list = new ArrayList<>();
 
-        List<Integer> newList = list.stream()
-                .filter(x -> x % 2 == 0)
-                .map(x -> x * 10).toList();
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
 
-        System.out.println(Arrays.toString(newList.toArray()));
+            double avg = list.stream()
+                    .map(p -> p.getPrice())
+                    .reduce(0.0, (x,y) -> x + y / list.size());
+
+            System.out.println("Average Price: " + String.format("%.2f", avg));
+
+            Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+
+            List<String> names = list.stream()
+                    .filter(p -> p.getPrice() < avg)
+                    .map(p -> p.getName())
+                    .sorted(comp.reversed()).toList();
+
+            names.forEach(System.out::println);
+
+
+        } catch (IOException e) {
+            System.out.println("Error" + e.getMessage());
+        }
 
     }
 }
